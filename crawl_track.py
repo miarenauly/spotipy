12e470ad4cf07b4c7f25dbfb2981b24b75c4079f
Mia Renauly <mia.renauly@dattabot.io>
2017-03-13 17:38:59 +0700

    insert to database

--- crawl_track.py
+++ crawl_track.py
@@ -7,10 +7,10 @@ import requests
 from mysql.connector import connect, Error
 
 
-dbhost = '192.168.10.9'
-dbuser = 'mia'
-dbpass = '161192'
-dbname = 'db_gis_test'
+dbhost = ''
+dbuser = ''
+dbpass = ''
+dbname = ''
 dbport = 3139
 
 conn = connect(host = dbhost,
@@ -133,8 +133,8 @@ def get_token(code,client_id, client_secret):
 '''
 
 def get_audio_feature(uri):
-    client_id = '5a84b178e67f4296823227c0d8e629fc'
-    client_secret = '04065cabc4d24b9eb551c19b3383e298'
+    client_id = ''
+    client_secret = ''
     uri = (str(uri)).replace(',','')
     client_credentials_manager = SpotifyClientCredentials(client_id,client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
